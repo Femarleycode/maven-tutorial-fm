d4f30942de8646f6fd12d21a3e843b158f431c38
femarleycode <femarleycode@gmail.com>
2020-04-08 16:57:26 +0100

    2nd commit

---
+++ src/main/java/com/qa/maventutorialfm/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.maventutorialfm;
+
+//import org.apache.commons.lang3.RandomUtils;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		System.out.println("HELLO THIS IS A TEST BY FM");
+//		System.out.println(RandomUtils.nextInt());
+	}
+
+}
